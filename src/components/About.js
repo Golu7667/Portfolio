db8eccf14bbce7d4659113f7bf41228127c818f1
unknown <goluchoudhary7808@gmail.com>
2023-07-20 19:31:04 +0530

    progress_day04_03

--- src/components/About.js
+++ src/components/About.js
@@ -4,23 +4,19 @@ import i from '../happy.json'
 import Typewriter from "typewriter-effect";
 
 function About() {
- 
-
-
   return (
     <>
-      <div className='mt-28  w-full  flex justify-end border-2 border-indigo-600 '>
-
+      <div className='mt-28  w-full  flex justify-end '>
         <div className='w-1/2 h-5/6  py-8'>
-          <div className='text-5xl font-bold mt-0 text-blue-800 my-6 ' style={{ fontFamily: 'Arvo' }}>Hi, I am Golu Choudhary  
-          <div className='text-3xl mt-6 ml-6'><Typewriter
-        options={{
-          strings: ["A Full Stack Developer", "A MERN Stack Developer" ],
-          autoStart: true,
-          loop: true,
-          deleteSpeed: 50,
-        }}
-      /></div>
+          <div className='text-5xl font-bold mt-0 text-blue-800 my-6 ' style={{ fontFamily: 'Arvo' }}>Hi, I am Golu Choudhary
+            <div className='text-3xl mt-6 ml-6'><Typewriter
+              options={{
+                strings: ["A Full Stack Developer", "A MERN Stack Developer"],
+                autoStart: true,
+                loop: true,
+                deleteSpeed: 50,
+              }}
+            /></div>
           </div>
           <div className='text-lg font-bold px-6 text-black' style={{ fontFamily: 'Arvo' }} >An inquisitive and highly motivated full stack web developer with specialization in MERN stack. Have relevant hands-on experience in building multiple websites from scratch. Seeking to pursue a career in a challenging environment to hone industry skill.</div>
         </div>
@@ -29,27 +25,27 @@ function About() {
         </div>
       </div>
       <div className='w-full flex justify-center text-3xl font-bold text-black'>About</div>
-      <div className='h-96 border-2 border-indigo-600 mt-2 flex '>
+      <div className='  h-96 mx-4 rounded-lg  shadow-lg shadow-black my-12 flex items-center'>
         {/* <div className='w-60 h-60 border-2 border-indigo-600 mt-8 ml-12'> */}
-        <img src="https://avatars.githubusercontent.com/u/103061012?s=400&u=8178651e4df3cf4728b69320b49df961f5226d84&v=4" className='w-60 h-60 border-2 border-indigo-600 mt-8 ml-12 rounded-2xl' />
+        <img src="https://avatars.githubusercontent.com/u/103061012?s=400&u=8178651e4df3cf4728b69320b49df961f5226d84&v=4" className='w-60 h-60 border mt-8 ml-12 rounded-2xl' />
         {/* </div> */}
         <div className='flex flex-col'>
-        <div className='text-xl font-bold mt-20 mx-10 text-indigo-400'>
+          <div className='text-xl font-bold mt-20 mx-10 text-indigo-400'>
 
-          Hi Everyone, I am Golu Choudhary from Bhagalpur, Bihar. I have completed my Higher Secondary Education at Sabour Collage Sabour,Bhagalpur.
+            Hi Everyone, I am Golu Choudhary from Bhagalpur, Bihar. I have completed my Higher Secondary Education at Sabour Collage Sabour,Bhagalpur.
 
-        </div>
-        <div className='mx-10 my-10 text-xl font-bold'>
-          Apart from coding, some other activities that I love to do!
-          <Typewriter
-        options={{
-          strings: ["A Full Stack Developer", "A MERN Stack Developer", "2000+ Hours Of Coding Experience","Solved aound 300+ DSA question" ],
-          autoStart: true,
-          loop: true,
-          deleteSpeed: 50,
-        }}
-      />
-        </div>
+          </div>
+          <div className='mx-10 my-10 text-xl font-bold'>
+            Apart from coding, some other activities that I love to do!
+            <Typewriter
+              options={{
+                strings: ["A Full Stack Developer", "A MERN Stack Developer", "2000+ Hours Of Coding Experience", "Solved aound 300+ DSA question"],
+                autoStart: true,
+                loop: true,
+                deleteSpeed: 50,
+              }}
+            />
+          </div>
         </div>
       </div>
 
--- src/components/Skills.js
+++ src/components/Skills.js
@@ -10,23 +10,24 @@ import {
   import { FaReact, FaNodeJs } from "react-icons/fa";
   import { DiCss3, DiMongodb } from "react-icons/di";
   import { SiJavascript } from "react-icons/si";
-  
+  import { BiLogoTailwindCss } from "react-icons/bi";
 
 function Skills() {
   return (
     <>
          <div className='flex justify-center text-3xl font-bold'>Skills</div>
-         <div className='my-12 flex justify-center gap-4  flex-wrap'>
-         <SiHtml5 className='h-28 w-28 bg-gray text-red-500 border-gray-700 rounded-2xl px-6 shadow-xl shadow-indigo-900' /> 
-         <DiCss3 className='h-28 w-28 text-blue-500 border-gray-700 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
-        <SiJavascript className='h-28 w-28 text-yellow-300 border-gray-700 rounded-2xl px-6  shadow-xl shadow-indigo-900   ' />
-         <FaReact className='h-28 w-28 text-blue-500 border-gray-700 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
-         <FaNodeJs  className='h-28 w-28 text-blue-500 border-gray-700 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
-         <DiMongodb className='h-28 w-28 text-blue-500 border-gray-700 rounded-2xl px-6 shadow-xl shadow-indigo-900 ' />
-         <SiRedux className='h-28 w-28 text-blue-500 border-gray-700 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
-         <SiExpress className='h-28 w-28 text-blue-500 border-gray-700 rounded-2xl px-6 shadow-xl shadow-indigo-900 ' />
-         <SiChakraui className='h-28 w-28 text-blue-500 border-gray-700 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
-         <SiChakraui className='h-28 w-28 text-blue-500 border-gray-700 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
+         <div className='my-12 flex justify-center gap-4 gap-y-12 flex-wrap'>
+         <SiHtml5 className='h-28 w-28 bg-gray text-red-500 rounded-2xl px-6 shadow-xl shadow-indigo-900' /> 
+         <DiCss3 className='h-28 w-28 text-blue-500 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
+        <SiJavascript className='h-28 w-28 text-yellow-300 rounded-2xl px-6  shadow-xl shadow-indigo-900   ' />
+         <FaReact className='h-28 w-28 text-blue-500 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
+         <FaNodeJs  className='h-28 w-28 text-green-500 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
+         <DiMongodb className='h-28 w-28 text-green-500 rounded-2xl px-6 shadow-xl shadow-indigo-900 ' />
+         <SiRedux className='h-28 w-28 text-purple-800 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
+         <SiExpress className='h-28 w-28 text-blue-500 rounded-2xl px-6 shadow-xl shadow-indigo-900 ' />
+         <SiChakraui className='h-28 w-28 text-green-500 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
+         <SiChakraui className='h-28 w-28 text-blue-500 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
+         <BiLogoTailwindCss className='h-28 w-28 text-blue-500 rounded-2xl px-6 shadow-xl shadow-indigo-900'/>
          </div>
     </>
     
