ae7cf7217594278cfc0c897f975ed4972b3ace10
unknown <goluchoudhary7808@gmail.com>
2023-07-20 17:44:36 +0530

    progress_day04_02

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -16,18 +16,17 @@ function Skills() {
   return (
     <>
          <div className='flex justify-center text-3xl font-bold'>Skills</div>
-         <div className='flex gap-4  flex-wrap'>
-        <SiJavascript className='h-36 w-36 border-2 text-red-500 border-red-700 rounded-2xl px-6  shadow-xl shadow-indigo-900   ' />
-        
-         <SiHtml5 className='h-36 w-36 border-2 text-blue-500 border-red-700 rounded-2xl px-6 shadow-xl shadow-indigo-900' /> 
-         <DiCss3 className='h-36 w-36 border-2 text-blue-500 border-red-700 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
-         <FaReact className='h-36 w-36 border-2 text-blue-500 border-red-700 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
-         <FaNodeJs  className='h-36 w-36 border-2 text-blue-500 border-red-700 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
-         <DiMongodb className='h-36 w-36 border-2 text-blue-500 border-red-700 rounded-2xl px-6 shadow-xl shadow-indigo-900 ' />
-         <SiRedux className='h-36 w-36 border-2 text-blue-500 border-red-700 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
-         <SiExpress className='h-36 w-36 border-2 text-blue-500 border-red-700 rounded-2xl px-6 shadow-xl shadow-indigo-900 ' />
-         <SiChakraui className='h-36 w-36 border-2 text-blue-500 border-red-700 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
-         <SiChakraui className='h-36 w-36 border-2 text-blue-500 border-red-700 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
+         <div className='my-12 flex justify-center gap-4  flex-wrap'>
+         <SiHtml5 className='h-28 w-28 bg-gray text-red-500 border-gray-700 rounded-2xl px-6 shadow-xl shadow-indigo-900' /> 
+         <DiCss3 className='h-28 w-28 text-blue-500 border-gray-700 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
+        <SiJavascript className='h-28 w-28 text-yellow-300 border-gray-700 rounded-2xl px-6  shadow-xl shadow-indigo-900   ' />
+         <FaReact className='h-28 w-28 text-blue-500 border-gray-700 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
+         <FaNodeJs  className='h-28 w-28 text-blue-500 border-gray-700 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
+         <DiMongodb className='h-28 w-28 text-blue-500 border-gray-700 rounded-2xl px-6 shadow-xl shadow-indigo-900 ' />
+         <SiRedux className='h-28 w-28 text-blue-500 border-gray-700 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
+         <SiExpress className='h-28 w-28 text-blue-500 border-gray-700 rounded-2xl px-6 shadow-xl shadow-indigo-900 ' />
+         <SiChakraui className='h-28 w-28 text-blue-500 border-gray-700 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
+         <SiChakraui className='h-28 w-28 text-blue-500 border-gray-700 rounded-2xl px-6 shadow-xl shadow-indigo-900' />
          </div>
     </>
     
