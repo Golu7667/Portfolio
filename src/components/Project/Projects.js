a958d0ed5cea946129357cbc961b226b0d3db97e
unknown <goluchoudhary7808@gmail.com>
2023-11-25 15:22:59 +0530

    update

--- src/components/Project/Projects.js
+++ src/components/Project/Projects.js
@@ -18,8 +18,8 @@ export const Projects = () => {
   const projects=[
     {id:'1',img:'https://i.postimg.cc/bw0CVpC7/flipkartimg.png',des:'Flipkart clone in MERN (MongoDB, Express, React, Node.js) - An e-commerce web application with admin and user roles, enabling users to browse and purchase products, manage their accounts, and make secure payments. The admin can manage products, categories, and orders, providing a comprehensive shopping experience.',name:'Flipkart Clone',deployurl:'https://flipkartmern.vercel.app/',githuburl:'https://github.com/Golu7667/Flipkart-Clone'},
     {id:'2',img:'https://i.postimg.cc/Hkxmgg3R/chatapp.png',des:'Develop a real-time MERN chat application utilizing Socket.io for seamless communication. This app integrates MongoDB, Express, React, and Node.js, enabling users to exchange messages instantly. Socket.io enhances the experience by providing live updates and smooth synchronization in a user-friendly interface.',name:'Chat App',deployurl:'https://chat-app-two-pi-40.vercel.app/',githuburl:'https://github.com/Golu7667/Chat-App'},
-    {id:'3',img:'https://i.postimg.cc/bw0CVpC7/flipkartimg.png',des:'',name:'',deployurl:'',githuburl:''},
-    {id:'4',img:'https://i.postimg.cc/bw0CVpC7/flipkartimg.png',des:'',name:'',deployurl:'',githuburl:''},
+    {id:'3',img:'https://i.postimg.cc/057K6LDb/videoapp.png',des:'A WebRTC project enhances real-time communication in a MERN (MongoDB, Express.js, React, Node.js) project, enabling seamless audio and video interactions, enriching the user experience.',name:'Video Streaming',deployurl:'https://videocall-mauve.vercel.app/',githuburl:'https://github.com/Golu7667/Video-streaming'},
+    {id:'4',img:'https://private-user-images.githubusercontent.com/103061012/282239696-c42c47f0-d785-4c42-b3ef-77fd62c6fc2d.PNG?jwt=eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJnaXRodWIuY29tIiwiYXVkIjoicmF3LmdpdGh1YnVzZXJjb250ZW50LmNvbSIsImtleSI6ImtleTEiLCJleHAiOjE3MDA5MDYwOTIsIm5iZiI6MTcwMDkwNTc5MiwicGF0aCI6Ii8xMDMwNjEwMTIvMjgyMjM5Njk2LWM0MmM0N2YwLWQ3ODUtNGM0Mi1iM2VmLTc3ZmQ2MmM2ZmMyZC5QTkc_WC1BbXotQWxnb3JpdGhtPUFXUzQtSE1BQy1TSEEyNTYmWC1BbXotQ3JlZGVudGlhbD1BS0lBSVdOSllBWDRDU1ZFSDUzQSUyRjIwMjMxMTI1JTJGdXMtZWFzdC0xJTJGczMlMkZhd3M0X3JlcXVlc3QmWC1BbXotRGF0ZT0yMDIzMTEyNVQwOTQ5NTJaJlgtQW16LUV4cGlyZXM9MzAwJlgtQW16LVNpZ25hdHVyZT1hZjJiYjhiM2NhNjYxYTE3MmI3Y2U5MjAzNzc5NDIyYjk3YWM1OTcwODA3M2U2MDI5YjY2ZjBjNjM4OWFmNGE5JlgtQW16LVNpZ25lZEhlYWRlcnM9aG9zdCZhY3Rvcl9pZD0wJmtleV9pZD0wJnJlcG9faWQ9MCJ9.Y1cLZ-bVxCIhZi9O_wF-ozAs78L_JDRB8H3JTXA6I-E',des:'In this mini project, i am implement Google OAuth 2.0 for a simplified login and signup process. Users can quickly log in or sign up using their Google accounts, ensuring a secure and user-friendly experience.',name:'Google-Outh2O',deployurl:'https://google-oauth-pearl.vercel.app/login',githuburl:'https://github.com/Golu7667/Google-Outh2O'},
     {id:'5',img:'https://i.postimg.cc/bw0CVpC7/flipkartimg.png',des:'',name:'',deployurl:'',githuburl:''},
     {id:'6',img:'https://i.postimg.cc/bw0CVpC7/flipkartimg.png',des:'',name:'',deployurl:'',githuburl:''},
     {id:'7',img:'https://i.postimg.cc/bw0CVpC7/flipkartimg.png',des:'',name:'',deployurl:'',githuburl:''},
@@ -68,11 +68,11 @@ export const Projects = () => {
               </div>
             </div>
             <div className="project_information" data-aos="fade-right">
-              <h2>{item.name}</h2>
+              <h2 style={{color:'blue'}}>{item.name}</h2>
               <p>
               {item.des}
               </p>
-              <div style={{display:'flex'}}>
+              <div style={{display:'flex',height:'auto'}}>
                 <FaReact />
                 <SiExpress />
                 <DiMongodb />
